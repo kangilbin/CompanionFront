d0ebd1086a2b333daeb92877f3281f5220ef612f
kangilbin <kib3388@naver.com>
2023-04-06 00:04:00 +0900

    메타태그 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,10 +30,31 @@ class MyDocument extends Document {
             httpEquiv="Content-Security-Policy"
             content="upgrade-insecure-requests"
           ></meta>
+          <meta
+            name="naver-site-verification"
+            content="a46aad79ff18796e502e2aafda992b17462d2d9c"
+          />
+          <meta
+            name="google-site-verification"
+            content="gW9YjrTjvGhDD5OE7l9st-7oN2N2yHrNFFj8O97V_u4"
+          />
+          <meta name="og:site_name" content="간택당한 집사s" />
+          <meta
+            name="og:description"
+            content="유기 동물 정보와 반려동물 관련 정보를 얻을 수 있는 커뮤니트 사이트입니다."
+          />
+          <meta
+            name="keywords"
+            content="유기동물, 반려동물, 입양, 보호, 반려동물 커뮤니티"
+          />
+          <meta
+            name="og:image"
+            content="https://source.unsplash.com/1600x900/?pet,care,issues"
+          />
           <script
             src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"
             async
-          />{" "}
+          />
           <script
             type="text/javascript"
             defer
