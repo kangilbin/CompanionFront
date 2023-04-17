9a0ea7c448e12e3c9c3ff3eb581fc8027e098024
kangilbin <kib3388@naver.com>
2023-04-17 17:54:07 +0900

    환경병수 테스트

--- common/firebase.tsx
+++ common/firebase.tsx
@@ -11,15 +11,5 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FRIEBASE_APP_MEASUREMENT_ID,
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDJvYB06cNNzVSDMUTyTIR6NQLbzn79UTU",
-//   authDomain: "companion-374309.firebaseapp.com",
-//   projectId: "companion-374309",
-//   storageBucket: "companion-374309.appspot.com",
-//   messagingSenderId: "743905169300",
-//   appId: "1:743905169300:web:4bec4e001cb4e9a17a8981",
-//   measurementId: "G-0B5BM5G8GN",
-// };
-
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
